2327e7832a94fca1458f01f38b69fa4c769b7050
GopirajaV <v.gopiraja95@gmail.com>
2015-11-12 07:30:09 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,13 @@
+n=str(input('Enter a character'))
+if(n=='a'):
+    print('Vowel')
+elif(n=='e'):
+    print('Vowel')
+elif(n=='i'):
+    print('Vowel')
+elif(n=='o'):
+    print('Vowel')
+elif(n=='u'):
+    print('Vowel')
+else:
+    print('Constant')
