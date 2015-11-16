08055c319d2101c336f16bae56efc7c6c15fd049
GopirajaV <v.gopiraja95@gmail.com>
2015-11-16 11:23:09 +0530

    Update Count_digits.py

--- Count_digits.py
+++ Count_digits.py
@@ -1,6 +1,9 @@
-n=int(input('Enter the integer'))
-count=0
-while(n>0):
-    n=int(n/10)
-    count+=1
-print(count)
+def main():
+    n = int(input('Enter the integer : '))
+    count = 0
+    while(n > 0):
+        n = int(n / 10)
+        count += 1
+    print(count)
+if __name__ == '__main__':
+    main()
