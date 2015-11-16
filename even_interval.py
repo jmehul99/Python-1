b725ad1c6edb2862b1e9c0a08067ef93ecfe91f8
GopirajaV <v.gopiraja95@gmail.com>
2015-11-16 20:21:11 +0530

    Create even_interval.py

---
+++ even_interval.py
@@ -0,0 +1,8 @@
+def display():
+    min = int(input("Enter the starting number : "))
+    max = int(input("Enter the ending number :"))
+    for i in range(min,max+1):
+        if(i % 2 == 0):
+            print(i)
+if __name__ == '__main__':
+    display()
