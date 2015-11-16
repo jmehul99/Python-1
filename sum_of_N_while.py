3f7799580d89dfa511c0d0e27a1167161e74f5af
GopirajaV <v.gopiraja95@gmail.com>
2015-11-16 10:40:19 +0530

    Update sum_of_N_while.py

--- sum_of_N_while.py
+++ sum_of_N_while.py
@@ -1,6 +1,9 @@
-n=int(input('Enter the limit'))
-x=0
-while(n>0):
-    x+=n
-    n-=1
-print(x)
+def main():
+    n = int(input('Enter the limit : '))
+    x = 0
+    while(n > 0):
+        x += n
+        n -= 1
+    print(x)
+if __name__ == '__main__':
+    main()
