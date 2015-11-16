05376908ffbc1296158a676ad140bade7ec8d005
GopirajaV <v.gopiraja95@gmail.com>
2015-11-16 10:19:09 +0530

    Update great_three.py

--- great_three.py
+++ great_three.py
@@ -1,13 +1,16 @@
-print('Enter the numbers')
-a=int(input())
-b=int(input())
-c=int(input())
-if(a>b):
-    if(a>c):
-        print(a,"is largest number")
+def main():
+    print('Enter three numbers : ')
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    if(a > b):
+        if(a > c):
+            print(a,"is largest number")
+        else:
+            print(c,"is Largest number")
+    elif(b > c):
+        print(b,"is Largest number")
     else:
         print(c,"is Largest number")
-elif(b>c):
-    print(b,"is Largest number")
-else:
-    print(c,"is Largest number")
+if __name__ == '__main__':
+    main()
