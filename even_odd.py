591e493e049bd7610401fd3f32a04fc9e7a4dd7e
GopirajaV <v.gopiraja95@gmail.com>
2015-11-16 10:24:15 +0530

    Create even_odd.py

---
+++ even_odd.py
@@ -0,0 +1,8 @@
+def main():
+    num = int(input("Enter a number"))
+    if(num % 2 == 0):
+        print("This is an even number")
+    else:
+        print("This is an odd number")
+if __name__ == '__main__':
+    main()
