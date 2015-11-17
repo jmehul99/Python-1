e18d6d455363dc129324e7ff983c6d4fb936fc3f
GopirajaV <v.gopiraja95@gmail.com>
2015-11-17 05:52:40 +0530

    Update armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -1,11 +1,16 @@
 def armstrong():
     num = int(input("Enter the number : "))
     temp = num
+    temp2 = num
     l=0
+    count = 0
+    while(temp2 > 0):
+        temp2 = int(temp2 / 10)
+        count += 1
     while(num > 0):
         z = num % 10
         num = int(num/10)
-        l=l+z**3
+        l=l+z**count
     if(temp == l):
         print("Armstrong Number")
     else:
