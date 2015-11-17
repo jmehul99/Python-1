522221532fead9b569205339992db79d083f5503
GopirajaV <v.gopiraja95@gmail.com>
2015-11-17 05:58:26 +0530

    Update armstrong_interval.py

--- armstrong_interval.py
+++ armstrong_interval.py
@@ -4,10 +4,15 @@ def armstrong():
     for num in range(min,max):
         l = 0
         temp = num
+        temp2 = num
+        count = 0
+        while(temp2 > 0):
+            temp2 = int(temp2 / 10)
+            count += 1
         while(num > 0):
             z = num % 10
             num = int(num/10)
-            l=l+z**3
+            l=l+z**count
         if(temp == l):
             print(l)
 if __name__ == '__main__':
