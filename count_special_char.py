7c4f4e3189a6987fc014228086a44881fc372f8b
GopirajaV <v.gopiraja95@gmail.com>
2015-11-19 10:22:54 +0530

    Create count_special_char.py

---
+++ count_special_char.py
@@ -0,0 +1,13 @@
+def main():
+    string = str(input("Enter the sring : "))
+    count = 0
+    l = list(string)
+    length = len(l)
+    for i in range(0,length):
+        x = str(l[i])
+        if(x.isalnum() == False):
+            count += 1
+    print("No. of Numeric character : %d"%count)
+
+if __name__ == '__main__':
+    main()
